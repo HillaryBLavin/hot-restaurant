4fefd2e787bda6b8c0e338a5e9dc6acadd6c9649
Ryan <Admin@MacBook-Pro.local>
2018-05-05 12:02:37 -0400

    food

--- server.js
+++ server.js
@@ -9,15 +9,9 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // parse application/json
 app.use(bodyParser.json())
  
-// app.use(function (req, res) {
-//   res.setHeader('Content-Type', 'text/plain')
-//   res.write('you posted: hello world\n')
-//   res.end(JSON.stringify(req.body, null, 2))
-//})
-app.get("/", function(req, res) {
-    res.send("Welcome to the Resturant!");
-  })
-  
+
+require('./app/routing/html-routes.js')(app);
+
 app.listen(PORT, function(){
     console.log("App listening on PORT: " + PORT);
 })
