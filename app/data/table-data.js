836cf6921cd3ac8efda22f8f776fedff8b342b6e
Ryan <Admin@MacBook-Pro.local>
2018-05-05 10:46:58 -0400

    hi again

---
+++ app/data/table-data.js
@@ -0,0 +1,11 @@
+var tableArray = [
+    {
+        customerName: 'Ryan',
+        customerEmail: 'ryanpatrick1@gmail.com',
+        customerID: 'ryan69',
+        phoneNumber: '555-555-5555'
+
+    }
+];
+
+module.exports = tableArray;
